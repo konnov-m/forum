de1e504d797c27a6fc4d661e3b4cc7d10e737ca2
Misha <strouk7@yandex.ru>
2021-10-10 15:33:44 +0300

    change static root

--- forum/settings.py
+++ forum/settings.py
@@ -119,14 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+# STATIC_URL = '/static/'
+#
+# # STATICFILES_DIRS = [
+# #    os.path.join(BASE_DIR, "static"),
+# # ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -136,3 +136,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login redirected url
 LOGIN_REDIRECT_URL = '/'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
