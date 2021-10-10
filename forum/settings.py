ee2dc847d7f1cd2ba1a3e310ef4ba7c105eccef5
Misha <strouk7@yandex.ru>
2021-10-10 15:22:30 +0300

    change static root

--- forum/settings.py
+++ forum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4c5)82re)&+w_%#!@#%))46+13%iy)nob0_qo2nr1yr9nj=@j$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
